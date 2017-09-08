fc3672b2bf10ad77a3720672f8dcb00ca2bd6117
Russell Hyde <russ_hyde@hotmail.com>
2017-09-08 15:56:39 +0100

    quartet group indicated by factor in anscombe

--- R/datasets.R
+++ R/datasets.R
@@ -10,7 +10,8 @@ NULL
 get_tidy_anscombe <- function(){
   ans <- datasets::anscombe
   tidy_df <- tibble::tibble(
-    group = rep(paste0("Q", 1:4), each = 11),
+    group = factor(rep(paste0("Q", 1:4), each = 11),
+                   levels = paste0("Q", 1:4)),
     x     = with(ans, c(x1, x2, x3, x4)),
     y     = with(ans, c(y1, y2, y3, y4))
   )
